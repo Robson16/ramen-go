8ea756eeef9f2c60947dcb4e5cc49e8dcec9998e
Robson Henrique Rodrigues <robhenrod@gmail.com>
2024-06-17 17:59:01 -0300

    feat: order success route

--- src/components/hero-section.js
+++ src/components/hero-section.js
@@ -6,21 +6,21 @@ class HeroSection extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
       <section class="hero">
-      <div class="container">
-        <img class="logo" src="${ramenGoLogo}" alt="Ramen Go!">
-        <div class="texts">
-          <span lang="ja">ラーメン</span>
-          <strong>GO!</strong>
-          <p>Enjoy a good ramen in the comfort of your house. Create your own ramen and choose your favorite flavour combination!</p>
-          <a href="#" class="cta">
-            ORDER NOW
-            <img src="${arrowRight}" alt="arrow right icon">
-          </a>
+        <div class="container">
+          <img class="logo" src="${ramenGoLogo}" alt="Ramen Go!">
+          <div class="texts">
+            <span lang="ja">ラーメン</span>
+            <strong>GO!</strong>
+            <p>Enjoy a good ramen in the comfort of your house. Create your own ramen and choose your favorite flavour combination!</p>
+            <a href="#" class="cta">
+              ORDER NOW
+              <img src="${arrowRight}" alt="arrow right icon">
+            </a>
+          </div>
+          <img class="illustration" src="${heroIllustration}" alt="Delivery girl">
         </div>
-        <img class="illustration" src="${heroIllustration}" alt="Delivery girl">
-      </div>
-      <!-- /.container -->
-    </section>
+        <!-- /.container -->
+      </section>
     `;
   }
 }
--- src/views/success-view.js
+++ src/views/success-view.js
@@ -1,9 +1,29 @@
+import ramenGoLogo from '/svg/ramen-go-logo.svg';
+import ramenImage from '/images/ramen.png'
+import arrowRight from '/svg/arrow-right.svg';
+import bowing from '/svg/bowing.svg';
+
 export function successView(description) {
   const appElement = document.getElementById('app');
 
   appElement.innerHTML = `
-    <h2>Order Successfully Created!</h2>
-    <p>Your order is a ${description}</p>
-    <a href="/">Back to Home</a>
+    <section class="success">
+      <img class="logo" src="${ramenGoLogo}" alt="Ramen Go!">
+      <div class="col">
+        <img class="illustration" src="${ramenImage}" alt="Ramen">
+        <span>Your Order:</span>
+        <strong>${description}</strong>
+      </div>
+      <div class="col">
+        <img class="bowing" src="${bowing}" alt="bowing">
+        <span lang="ja">どもありがとうございます。</span>
+        <strong>Your order is being prepared</strong>
+        <p>Hold on, when you least expect you will be eating your rámen.</p>
+        <a href="/" class="cta">
+          PLACE NEW ORDER
+          <img src="${arrowRight}" alt="arrow right icon">
+        </a>
+      </div>
+    </section>
   `;
-}
+} 
