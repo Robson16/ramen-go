a65da9896391fec9afe222d51ed3db7df35b48c1
Robson H. Rodrigues <robhenrod@gmail.com>
2026-08-27 12:42:08 -0300

    feat: remove api key and prepare axios for jwt

--- src/app/_lib/axios.ts
+++ src/app/_lib/axios.ts
@@ -2,12 +2,18 @@ import axios from 'axios'
 
 import { env } from '@/app/env'
 
-const apiKey = env.NEXT_PUBLIC_API_KEY
 const apiBaseUrl = env.NEXT_PUBLIC_API_BASE_URL
 
 export const api = axios.create({
   baseURL: apiBaseUrl,
-  headers: {
-    'x-api-key': apiKey,
-  },
+})
+
+api.interceptors.request.use((config) => {
+  const token = null
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`
+  }
+
+  return config
 })
